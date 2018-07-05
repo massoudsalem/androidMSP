b8091bf3abc76a7dc66009b2d42eb96b8bdf9274
massoudsalem <massoudsalem@users.noreply.github.com>
2018-07-05 21:37:34 +0200

    removeActionBar

--- UpcomingMovies/app/src/main/java/com/example/mohmassoud/upcomingmovies/MainActivity.java
+++ UpcomingMovies/app/src/main/java/com/example/mohmassoud/upcomingmovies/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         itemList = new ArrayList<>();
         new apiTMDB().execute();
